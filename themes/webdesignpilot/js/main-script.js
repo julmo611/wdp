cc7b7baaf8c4d84462e1431027f532c573ac531f
Julian Moreno <julian@webdesignpilot.com>
2018-05-08 20:09:15 -0700

    Old page Main Template

---
+++ themes/webdesignpilot/js/main-script.js
@@ -0,0 +1,15 @@
+
+// (function($) {
+// Drupal.behaviors.myBehavior = {
+//   attach: function () {
+
+//     //code starts
+//     $("body").click(function() {
+//       alert("Hello World");
+//     });
+//     //code ends
+
+//   }
+// };
+
+// })(jQuery);
